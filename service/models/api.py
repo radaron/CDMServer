8bf86f7672cddf2a244a8d4ab08617032c80cc66
Aron Radics <ajradics@yext.com>
2025-01-03 18:08:50 +0100

    WIP

--- service/models/api.py
+++ service/models/api.py
@@ -5,7 +5,7 @@ from pydantic.alias_generators import to_camel
 class BaseData(BaseModel):
     model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True, model_dump_by_alias=True)
 
-    def model_dump(self, **kwargs):
+    def model_dump(self, **kwargs) -> dict:
         return super().model_dump(by_alias=True, **kwargs)
 
 
