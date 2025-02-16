0cb5d5147fc66cc9341c5807a1991f5278c7bd4d
Aron Radics <ajradics@yext.com>
2025-02-16 16:38:25 +0100

    Add device sharing support for frontend
    And add translation for dowlload folder settings

--- frontend/src/Manage/Device/SettingsModal/SettingsModal.jsx
+++ frontend/src/Manage/Device/SettingsModal/SettingsModal.jsx
@@ -1,8 +1,10 @@
-import { Button, Modal, Form } from 'react-bootstrap'
+import { Button, Modal, Form, Container, Row, Col } from 'react-bootstrap'
+import { PersonFillDash, PersonFillAdd } from 'react-bootstrap-icons';
 import { useContext } from "react"
 import { manageContext } from "../../Manage"
 import { useTranslation } from "react-i18next"
 import { LOGIN_PAGE, redirectToPage } from "../../../util"
+import { DownloadFolders } from "../../constant"
 
 export const SettingsModal = ({ data, setData }) => {
   const { t } = useTranslation()
@@ -19,6 +21,7 @@ export const SettingsModal = ({ data, setData }) => {
           },
           body: JSON.stringify({
               settings: data.settings,
+              userEmails: data.userEmails,
           })
       })
       if (resp.status === 200) {
@@ -39,25 +42,76 @@ export const SettingsModal = ({ data, setData }) => {
   }
 
   return (
-    <Modal show={data?.settings && data.settings.length > 0} onHide={handleClose}>
+    <Modal show={data.name} onHide={handleClose} size='xl'>
       <Modal.Header closeButton>
         <Modal.Title>{t('DEVICE_SETTINGS_TITLE')}</Modal.Title>
       </Modal.Header>
       <Form onSubmit={handleSave}>
         <Modal.Body>
-            {data?.settings && data.settings.map((setting, index) => (
-              <Form.Group key={setting.name} className="mb-3" controlId={setting.name}>
-                <Form.Label>{setting.title}</Form.Label>
-                <Form.Control
-                  value={setting.value}
-                  onChange={(event) => {
-                    const newData = {...data}
-                    newData.settings[index].value = event.target.value
-                    setData(newData)
-                  }}
-                />
-              </Form.Group>
-            ))}
+          <Container>
+            <Row>
+              <Col>
+                {data?.settings && Object.entries(data.settings).map(([key, value]) => (
+                  <Form.Group key={key} className="mb-3" controlId={key}>
+                    <Form.Label>{t(DownloadFolders[key])}</Form.Label>
+                    <Form.Control
+                      value={value}
+                      onChange={(event) => {
+                        const newData = {...data}
+                        newData.settings[key] = event.target.value
+                        setData(newData)
+                      }}
+                    />
+                  </Form.Group>
+                ))}
+              </Col>
+              <Col>
+                <Row>
+                  <Col xs={10}>
+                    <Form.Label>{t('DEVICE_OWNERS_TITLE')}</Form.Label>
+                  </Col>
+                  <Col xs={2}>
+                    <Button
+                      variant="outline-success"
+                      onClick={() => {
+                        const newData = {...data}
+                        newData.userEmails.push('')
+                        setData(newData)
+                      }}>
+                      <PersonFillAdd/>
+                    </Button>
+                  </Col>
+                </Row>
+                {data?.userEmails && data.userEmails.map((mail, index) => (
+                  <Row key={index} className="mt-3">
+                    <Col xs={10}>
+                      <Form.Group controlId={mail}>
+                        <Form.Control
+                          value={mail}
+                          onChange={(event) => {
+                            const newData = {...data}
+                            newData.userEmails[index] = event.target.value
+                            setData(newData)
+                          }}
+                        />
+                      </Form.Group>
+                    </Col>
+                    <Col xs={2}>
+                      <Button
+                        variant="outline-danger"
+                        onClick={() => {
+                          const newData = {...data}
+                          newData.userEmails.splice(index, 1)
+                          setData(newData)
+                        }}>
+                        <PersonFillDash/>
+                      </Button>
+                    </Col>
+                  </Row>
+                ))}
+              </Col>
+            </Row>
+          </Container>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>{t('DEVICE_SETTINGS_CLOSE')}</Button>
