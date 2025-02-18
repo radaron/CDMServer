ec3971c94f30b35717351f017490cb4f8b3486bd
Aron Radics <ajradics@yext.com>
2025-02-19 00:07:04 +0100

    Fix user deletion

--- frontend/src/Manage/Admin/DeleteUser/DeleteUser.jsx
+++ frontend/src/Manage/Admin/DeleteUser/DeleteUser.jsx
@@ -1,5 +1,5 @@
 import { Form, Button } from 'react-bootstrap'
-import { useState, useContext } from 'react'
+import { useState, useContext, useEffect } from 'react'
 import { manageContext } from '../../Manage'
 import './DeleteUser.css'
 import { useTranslation } from 'react-i18next'
@@ -7,8 +7,12 @@ import { LOGIN_PAGE, redirectToPage } from '../../../util'
 
 export const DeleteUser = ({ fetchUsers, users }) => {
   const { t } = useTranslation()
-  const [selectedUser, setSelectedUser] = useState(users[0]?.email)
+  const [selectedUser, setSelectedUser] = useState('')
   const { setToastData } = useContext(manageContext)
+
+  useEffect(() => {
+    setSelectedUser(users[0]?.email)
+  }, [users])
 
   const handleDelete = async (event) => {
     event.preventDefault()
