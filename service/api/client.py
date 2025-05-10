3a7ce5d4cde17a6bf59eb9792d049d095127afff
Aron Radics <ajradics@yext.com>
2025-05-10 14:20:42 +0200

    Add redis to store download statuses

--- service/api/client.py
+++ service/api/client.py
@@ -4,7 +4,8 @@ from datetime import datetime, timezone
 from fastapi.responses import JSONResponse, FileResponse
 from fastapi import APIRouter, Depends, Header
 from service.models.api import StatusData
-from service.models.database import AsyncSession, get_session, select, delete, Device, Torrent
+from service.models.database import AsyncSession, get_session, select, delete, Device
+from service.torrents_adapter import TorrentsAdapter, TorrentStatus
 
 
 router = APIRouter()
@@ -59,34 +60,18 @@ async def add_device(data: StatusData, session: AsyncSession = Depends(get_sessi
     if device is None:
         return JSONResponse({"message": "Unathorized"}, status_code=401)
 
-    await session.execute(delete(Torrent).where(Torrent.device_id == device.id))
-
+    torrents_adapter = TorrentsAdapter()
     for item in data.data:
-        result = await session.execute(
-            select(Torrent).where(Torrent.device_id == device.id, Torrent.torrent_id == item.id)
-        )
-        torrent = result.scalars().first()
-        if torrent is None:
-            added_date = datetime.fromtimestamp(item.added_date, tz=timezone.utc)
-            new_torrent = Torrent(
-                device_id=device.id,
-                torrent_id=item.id,
-                name=item.name,
-                added_date=added_date,
-                status=item.status,
-                progress=item.progress,
-                download_dir=item.download_dir,
-                total_size=item.total_size,
-                eta=item.eta,
-            )
-            session.add(new_torrent)
-        else:
-            torrent.status = item.status
-            torrent.progress = item.progress
-            torrent.download_dir = item.download_dir
-            torrent.total_size = item.total_size
-            torrent.eta = item.eta
-            session.add(torrent)
-    await session.commit()
+        torrent = TorrentStatus.model_validate({
+            "id": item.id,
+            "name": item.name,
+            "status": item.status,
+            "progress": item.progress,
+            "download_dir": item.download_dir,
+            "added_date": item.added_date,
+            "total_size": item.total_size,
+            "eta": item.eta,
+        })
+        await torrents_adapter.set_torrent(device.id, torrent)
 
     return JSONResponse({"message": "Status updated"}, status_code=200)
