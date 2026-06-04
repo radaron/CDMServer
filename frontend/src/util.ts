45496426975cf3c0c88d38babe8dd58168bf2fad
Aron Radics <ajradics@yext.com>
2026-06-04 20:05:45 +0200

    Fix some ui bug

--- frontend/src/util.ts
+++ frontend/src/util.ts
@@ -26,13 +26,6 @@ export const toggleLanguage = () => {
 export const separateWords = (text: string): string => {
   return text
     .split(/[\s\-.]+/)
-    .flatMap((word) => {
-      const chunks: string[] = []
-      for (let i = 0; i < word.length; i += 10) {
-        chunks.push(word.slice(i, i + 10))
-      }
-      return chunks.join('-')
-    })
     .join(' ')
 }
 
